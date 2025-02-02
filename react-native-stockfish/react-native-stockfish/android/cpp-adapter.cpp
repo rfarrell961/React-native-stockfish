556d0510888bb27f4ac75c79c2d810f823ee42f6
rfarrell961 <rff961@gmail.com>
2025-02-01 21:47:34 -0500

    Clean up project

--- react-native-stockfish/react-native-stockfish/android/cpp-adapter.cpp
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-#include "react-native-stockfish.h"
-
-extern "C"
-JNIEXPORT jdouble JNICALL
-Java_com_stockfish_StockfishModule_nativeMultiply(JNIEnv *env, jclass type, jdouble a, jdouble b) {
-    return stockfish::multiply(a, b);
-}
--- react-native-stockfish/react-native-stockfish/example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  StockfishExample
-//
-
-import Foundation
